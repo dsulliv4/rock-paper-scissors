b66b145efc1fd1aa407ca9455fa6c4bb5dce3831
Deirdre Sullivan <drsullivan93@gmail.com>
2022-09-29 05:28:38 -0400

    computer choice javascript

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice= e.target.id
     userChoiceDisplay.innerHTML = userChoice 
+    generateComputerChoice()
 })) 
 
 
 function generateComputerChoice() {
-    const randomNumber = Math.random() * possibleChoices.length
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
 }
